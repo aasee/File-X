22f7bd92e2fd0e518aab27d16d18041311186c63
unknown <thirumalai899@gmail.com>
2018-03-12 22:11:02 +0530

    cross origin added

--- src/main/java/com/tvg/filex/FileX/RestApi.java
+++ src/main/java/com/tvg/filex/FileX/RestApi.java
@@ -12,6 +12,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class RestApi {
 
 	@GetMapping("/files")
